08cd69b7c7742ac2d3ffe57184e0a65b4aa8639b
Itallo Rosario <itallopuliafico@hotmail.com>
2025-08-24 18:10:45 -0300

    teste itallo

---
+++ src/main/java/com/pi/projeto_quarto_semestre/ProjetoQuartoSemestreApplication.java
@@ -0,0 +1,13 @@
+package com.pi.projeto_quarto_semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoQuartoSemestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoQuartoSemestreApplication.class, args);
+	}
+
+}
